43e0753aa7abe9acd4fe399102278826106af57e
Batname <dadubinin@gmail.com>
2016-03-20 11:15:08 +0100

    add index controller

--- cs-ios-webView/IndexController.swift
+++ cs-ios-webView/IndexController.swift
@@ -1,14 +1,7 @@
-//
-//  ViewController.swift
-//  cs-ios-webView
-//
-//  Created by Денис Дубинин on 3/20/16.
-//  Copyright © 2016 Denis Dubinin. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class IndexController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

53d92683a107f51188e3e00af22045ba07b44c2f
Batname <dadubinin@gmail.com>
2016-03-20 10:44:39 +0100

    Initial Commit

---
+++ cs-ios-webViewTests/cs_ios_webViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  cs_ios_webViewTests.swift
+//  cs-ios-webViewTests
+//
+//  Created by Денис Дубинин on 3/20/16.
+//  Copyright © 2016 Denis Dubinin. All rights reserved.
+//
+
+import XCTest
+@testable import cs_ios_webView
+
+class cs_ios_webViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

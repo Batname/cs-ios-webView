6405f81798ae0e2d23352b70c33348bafd5740d2
Batname <dadubinin@gmail.com>
2016-03-30 21:28:49 +0200

    add progressView

--- cs-ios-webView/Controllers/IndexController.swift
+++ cs-ios-webView/Controllers/IndexController.swift
@@ -7,6 +7,8 @@ class IndexController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView
     let website = "http://localhost:8000"
     
+    @IBOutlet weak var progressView: UIProgressView!
+    
     func webView(webView: WKWebView,
         didStartProvisionalNavigation navigation: WKNavigation){
             print("Loading START")
@@ -15,6 +17,7 @@ class IndexController: UIViewController, WKNavigationDelegate {
     func webView(webView: WKWebView,
         didFinishNavigation navigation: WKNavigation){
             print("Loading DONE")
+            self.progressView.setProgress(0.0, animated: false)
             self.layoutWebBrowsingElements()
             webView.evaluateJavaScript("console.log('Loading DONE')", completionHandler: nil)
     }
@@ -34,8 +37,16 @@ class IndexController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
         self.webView.navigationDelegate = self
         self.loadURLRequest(self.website)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if (keyPath == "estimatedProgress") {
+            self.progressView.hidden = self.webView.estimatedProgress == 1
+            self.progressView.setProgress(Float(self.webView.estimatedProgress), animated: true)
+        }
     }
     
     func loadURLRequest(address: String) {
@@ -48,7 +59,7 @@ class IndexController: UIViewController, WKNavigationDelegate {
         
         self.webView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(webView)
+        self.view.addSubview(self.webView)
         
         let webTopSpaceToContainer = NSLayoutConstraint(item: self.view, attribute: .TopMargin, relatedBy: .Equal, toItem: webView, attribute: .Top, multiplier: 1, constant: -20)
         let webBottomSpaceToContainer = NSLayoutConstraint(item: self.view, attribute: .BottomMargin, relatedBy: .Equal, toItem: webView, attribute: .Bottom, multiplier: 1, constant: 0)

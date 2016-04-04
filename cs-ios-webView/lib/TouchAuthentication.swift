c46d4e59f2b7da33751aaedd2a7ddc13525b757d
Batname <dadubinin@gmail.com>
2016-04-04 15:24:39 +0200

    start implementation TouchAuthentication

---
+++ cs-ios-webView/lib/TouchAuthentication.swift
@@ -0,0 +1,13 @@
+//
+//  TouchAuthentication.swift
+//  cs-ios-webView
+//
+//  Created by Денис Дубинин on 4/3/16.
+//  Copyright © 2016 Denis Dubinin. All rights reserved.
+//
+
+import LocalAuthentication
+
+class TouchAuthentication {
+
+}

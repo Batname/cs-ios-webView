303bab33c9d88bc7b8f9bf3497467f26374ca01d
Batname <dadubinin@gmail.com>
2016-04-16 13:00:07 +0200

    add needed files

---
+++ cs-ios-webView/Services/NotificationReminder.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationReminder.swift
+//  cs-ios-webView
+//
+//  Created by Денис Дубинин on 4/16/16.
+//  Copyright © 2016 Denis Dubinin. All rights reserved.
+//
+
+import Foundation
